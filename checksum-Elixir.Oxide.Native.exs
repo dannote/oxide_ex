cd73a89da858baa67fafb79d25738afcfbd26dfc
Danila Poyarkov <dev@dannote.net>
2026-03-12 11:11:16 +0300

    Add checksum file for precompiled NIFs

---
+++ checksum-Elixir.Oxide.Native.exs
@@ -0,0 +1,7 @@
+%{
+  "liboxide_ex_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:bb16a429698458e533eadba5dc142a3fb8b6a9693f9c3b9432f60712e1a2ccda",
+  "liboxide_ex_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:b2b9e559f78c4e9d8f02e281c4d75952b50efb7285c6df171c3bb8235731593a",
+  "liboxide_ex_nif-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:96657dc3adad869f182905d236f43c43a02b6ac1bd1a62f8a446932683222ccb",
+  "liboxide_ex_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:059358df232d50a803e91d8f0e030e82eb8610b03d91d88048b5074ae5b06145",
+  "liboxide_ex_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:764744794d3e712d1cb008d7320e0ee77400fc45d953070f35dbffdb55bdbd6e",
+}
